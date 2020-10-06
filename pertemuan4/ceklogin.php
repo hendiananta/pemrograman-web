8c8862d200f454f14dd5e7dd0a1c1ead28d25ba6
hendiananta <hendiananta@gmail.com>
2020-10-06 14:39:47 +0700

    membuat session login

--- pertemuan4/ceklogin.php
+++ pertemuan4/ceklogin.php
@@ -1,11 +1,14 @@
 <?php
+session_start();
 
 if (isset($_POST['tombolSubmit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     if ($username == "anto" && $password == "123") {
-        echo "Sukses";
+        //echo "Sukses";
+        $_SESSION["sessionUsername"] = $username;
+        header("Location: dashboard.php");
     }
     else {
         echo "Username / Password ada yg salah";
@@ -14,4 +17,6 @@ if (isset($_POST['tombolSubmit'])) {
 else {
  echo "Mohon maaf cek login tidak bs diakses langsung";
 }
+
+
 ?>
